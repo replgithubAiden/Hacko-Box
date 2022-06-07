d493dd155042a78183e5680796e4f9e024a0686e
replgithubAiden <92238811+replgithubAiden@users.noreply.github.com>
2022-06-07 09:16:45 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 sudo apt install hollywood
 sudo apt install httrack webhhttrack
 sudo apt install hping3
+sudo apt install figlet
+sudo apt install python3
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
+echo loading boot system...
+sleep 1m
+echo applications unzipping...
+sleep 40
